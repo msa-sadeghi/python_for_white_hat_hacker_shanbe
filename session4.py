fe329d5e1454bf0ed993ebdc359c159002bc28d5
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-07-26 18:02:52 +0330

    session4

---
+++ session4.py
@@ -0,0 +1,19 @@
+# import socket
+
+
+# target_ip = input("enter the ip: ")
+# ports = [445, 135]
+
+# for port in ports:
+#     sock =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     result = sock.connect_ex((target_ip, port))
+#     sock.settimeout(0.5)
+#     if result == 0:
+#         print(f"port {port} is open!")
+#     sock.close()
+
+
+import requests
+ip = input("enter the ip: ")
+response = requests.get(f"http://ip-api.com/json/{ip}")
+print(response.text)
