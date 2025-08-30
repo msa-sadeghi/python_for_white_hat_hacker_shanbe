9b4306c6f86b0151add073813832a1f494decbd6
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-30 17:59:54 +0330

    ccc

--- session7.py
+++ session7.py
@@ -7,6 +7,14 @@ from time import sleep
 app = Flask("myapp")
 html_form = '''
 <p style="color:red">{{message}}</p>
+{% if show_captcha %}
+
+<img src="{{url_for('captcha_image', text='something')}}" alt="CAPTCHA">
+<br>
+captcha <input type="text" name="captcha">
+<br>
+{% endif %}
+
 <form method="POST">
 username:<input type="text" name="username"><br>
 password:<input type="password" name="password"><br>
@@ -15,7 +23,8 @@ password:<input type="password" name="password"><br>
 '''
 
 def generate_captcha_text(length=5):
-    return ''.join(random.choice(string.ascii_uppercase + string.digits , k = length))
+    return ''.join(random.choices(string.ascii_uppercase +\
+                                   string.digits , k = length))
 
 def generate_captcha_image(text):
     img = Image.new('RGB', (150,60), color=(255,255,255))
@@ -26,6 +35,7 @@ def generate_captcha_image(text):
         font = ImageFont.load_default()
 
     d.text((10,10), text, font=font, fill=(0,0,0))
+    img = img.rotate(30, expand=1)
     buf = io.BytesIO()
     img.save(buf, format='PNG')
     buf.seek(0)
@@ -33,30 +43,41 @@ def generate_captcha_image(text):
 
 
 @app.route("/captcha.png")
-def captcha_image(text):
-    img = generate_captcha_image(text)
+def captcha_image():
+    img = generate_captcha_image(captcha_text)
     return send_file(img, mimetype='image/png')
 
 
-
+captcha_text = ""
 correct_username = "user1"
 correct_password = "pass1"
 attempts = 0
 @app.route("/home/", methods=["GET", "POST"])
 def login():
     global attempts
+    global captcha_text
     attempts += 1
     sleep(1)
     message = ""
+    show_captcha = False
+    
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
         if attempts > 3:
+            show_captcha = True
             message = "Captcha"
+            captcha_text = generate_captcha_text()
+            
+            user_captcha = request.form.get("captcha", "")
+            print("captcha_text", captcha_text)
+            print("user_captcha", user_captcha)
+            if captcha_text == user_captcha:
+                print("captcha ok")
         elif username == correct_username and \
         password == correct_password:
             message =  "login success"
         else:
             message =  "login failed"    
-    return render_template_string(html_form, message= message)
+    return render_template_string(html_form, message= message, show_captcha=show_captcha)
 app.run(debug=True)
